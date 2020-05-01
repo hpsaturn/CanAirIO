26185086bf31592fdf0fa296c14be981995a3252
Antonio Vanegas <hpsaturn@gmail.com>
2020-05-01 23:28:03 +0200

    isoloted buttons actions in separated files

---
+++ src/buttons.cpp
@@ -0,0 +1,42 @@
+#include <buttons.hpp>
+
+// Button2 btn1(BUTTON_1);
+// Button2 btn2(BUTTON_2);
+
+void btnActionDeepSleep() {
+    Serial.println("-->[btnActionDeepSleep]");
+    displayOff();
+    espDeepSleep();
+}
+
+void btnActionVoltagePage() {
+    Serial.println("-->[btnActionVoltagePage]");
+    showVoltagePage();
+}
+
+// void buttonLoop() {
+//     btn1.loop();
+//     btn2.loop();
+// }
+
+void btnInit() {
+
+    pinMode(BUTTON_2, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(BUTTON_2), btnActionDeepSleep, FALLING);
+
+    // btn1.setLongClickHandler([](Button2 &b) {
+    //     btn1Click();
+    // });
+
+    // btn1.setClickHandler([](Button2 &b) {
+    // });
+
+    // btn2.setLongClickHandler([](Button2 &b) {
+    //     Serial.println("-->[Btn2 Detect:] Wifi Scan..");
+    //     wifi_scan();
+    // });
+
+    // btn2.setClickHandler([](Button2 &b) {
+    //     showVoltage();
+    // });
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,12 @@
-#include <Button2.h>
 #include <sleep.hpp>
 #include <gui.hpp>
+#include <buttons.hpp>
 #include <hal.hpp>
-
 #include "WiFi.h"
-// #include <vector>
-
-// Button2 btn1(BUTTON_1);
-// Button2 btn2(BUTTON_2);
 
 char buff[512];
 long count = 0;
-bool btn1click;
 bool lowPowerMode;
-
-void btn1Click() {
-    Serial.println("-->[Btn1LngPress:] Suspend..");
-    displayOff();
-    espDeepSleep();
-}
-
-void btn2Click() {
-    Serial.println("-->[Btn2 Detect:] Voltage..");
-    showVoltagePage();
-}
-
-// void buttonLoop() {
-//     btn1.loop();
-//     btn2.loop();
-// }
-
-void buttonInit() {
-
-    pinMode(BUTTON_2, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(BUTTON_2), btn1Click, FALLING);
-
-    // btn1.setLongClickHandler([](Button2 &b) {
-    //     btn1Click();
-    // });
-
-    // btn1.setClickHandler([](Button2 &b) {
-    // });
-
-    // btn2.setLongClickHandler([](Button2 &b) {
-    //     Serial.println("-->[Btn2 Detect:] Wifi Scan..");
-    //     wifi_scan();
-    // });
-
-    // btn2.setClickHandler([](Button2 &b) {
-    //     showVoltage();
-    // });
-}
 
 void setup() {
     Serial.begin(115200);
@@ -58,7 +14,7 @@ void setup() {
     displayInit();
     showMainPage();
     pmsensorInit();
-    buttonInit();
+    btnInit();
     setupBattery();
     setupBattADC();
     bmeInit();
